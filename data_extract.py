89ca18ddb969c623f87c5bdf9bbdf24a5fe90b53
Anmol Parande <parandea1.7@gmail.com>
2018-08-23 22:36:46 -0700

    Initial Commit

---
+++ data_extract.py
@@ -0,0 +1,84 @@
+import csv
+import string
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+
+from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+
+import pickle
+
+"""
+Takes a list of statistics about basketball teams and opens the corresponding .csv file
+Compiles these files into a single dictionary 
+Team name is the key and each value is a dictionary with a key of the statistic and the value
+"""
+def constructKnowledge(files):
+    dataSet = dict()
+    for name in files:
+        csvFile = open('Data/'+name+'.csv', 'rU')
+        reader = csv.reader(csvFile)
+        for row in reader:
+            #If the knowledgebase does not have the current team, create an empty dictionary for it
+            if row[0] not in dataSet:
+                dataSet[row[0]] = dict()
+            #Place the value for the statistic for the appropriate team in the dictionary
+            dataSet[row[0]][name] = row[1]
+
+    return dataSet
+
+"""
+Breaks the compiled data into features.
+Each feature is calculated by substracting the statistic for the second team listed from the first team listed
+Each label is either a 1 or a 2 indicating that "Team One" won or "Team Two" won
+"""
+def breakData():
+    csvFile = open('Data/gameData.csv', 'rU')
+    reader = csv.reader(csvFile)
+
+    features = []
+    labels = []
+    i = 0
+    for row in reader:
+        if (i == 0):
+            i += 1
+            continue
+        labels.append(row[2])
+        space = [float(row[3]) - float(row[9]), float(row[4]) - float(row[10]), float(row[5]) - float(row[11]), float(row[6]) - float(row[12]), float(row[7]) - float(row[13]), float(row[8]) - float(row[14])]
+        features.append(space)
+    return features, labels
+
+
+from sklearn.model_selection import cross_val_score
+
+#Cross validates the classifier to check accuracy
+def check_classifier(clf, features, labels):
+    #Computes scores using 10-fold crossvalidation
+    scores = cross_val_score(clf, features, labels, cv=10)
+    print scores
+    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std()))
+    print "\n"
+
+#Loads the classifer from a pickle
+def load_classifier(name="optimal.pkl"):
+    return pickle.load(open(name))
+
+#Helper function to save the classifier to a pickle file
+def save_classifier(clf, features, labels, name="optimal.pkl"):
+    clf.fit(features, labels)
+    pickle.dump(clf, open(name, 'w+'))
+
+
+def main():
+    features, labels = breakData()
+    #Train an SVM on with these parameters
+    svc = svm.SVC(kernel='linear', C=1000, gamma=0.1)
+    check_classifier(svc, features, labels)
+    save_classifier(svc, features, labels)
+
+if __name__ == "__main__":
+    main()
---
+++ execute.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python -W ignore::DeprecationWarning
+
+import warnings
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    import sklearn
+    import data_extract as de
+
+def load():
+    #Load points per game (ppg), offensive efficiency (oe), defensive efficiency (de), 
+    # field goal efficiency (fge), offensive rebounds (or), and rpi
+    knowledge = de.constructKnowledge(['ppg', 'oe', 'de', 'fge', 'or', 'rpi'])
+    clf = de.load_classifier()
+    return knowledge, clf
+
+#Returns the corresponding statistics for team queried
+def query(team, knowledge):
+    if team not in knowledge:
+        return False
+
+    teamData = knowledge[team]
+    return [teamData['ppg'], teamData['oe'], teamData['de'], teamData['fge'], teamData['or'], teamData['rpi']]
+
+#Calculates the features of the selected game by subtracting the statistics of the two teams
+def gameData(one_data, two_data):
+    data = []
+    for row in range(0, len(one_data)):
+        data.append(float(one_data[row]) - float(two_data[row]))
+    return data
+
+def prediction(team_one, team_two, knowledge, clf):
+    dataOne = query(team_one, knowledge)
+    dataTwo = query(team_two, knowledge)
+    if not dataOne:
+        return "Could not find %s in knowledgebase" % team_one
+    if not dataTwo:
+        return "Could not find %s in knowledgebase" % team_two
+
+    #Calculate the feature
+    feature_space = gameData(dataOne, dataTwo)
+    #Use the classifier to predict the winner. 1 for team one and 2 for team two
+    pred = clf.predict(feature_space)
+    if pred[0] == '1':
+        return team_one
+    else:
+        return team_two
+
+#Create a bracket by entering each matchup in the tournament
+def question(clf, knowledge):
+    team_one = raw_input("Enter Team One: ")
+    team_two = raw_input("Enter Team Two: ")
+
+    message = prediction(team_one, team_two, knowledge, clf)
+    print message
+    shouldContinue = raw_input("Do you want to continue (Y/N): ")
+    if shouldContinue == "Y":
+        question(clf, knowledge)
+    else:
+        return
+
+#Load the knowledge and the classifier
+knowledge, clf = load()
+question(clf, knowledge)
